5169561accc211bc3c0a857cf7a5bc56612fc77e
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-09 01:36:36 +0200

    asd

--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -1,8 +1,22 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+// import PropTypes from 'prop-types';
+import React, {useEffect, useState} from 'react';
 import MediaRow from './MediaRow';
 
-const MediaTable = ({mediaArray}) => {
+const MediaTable = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+  const getMediaFetch = async () => {
+    try {
+      const response = await fetch('test.json');
+      const data = await response.json();
+      setMediaArray(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getMediaFetch();
+  }, []);
+
   console.log(mediaArray);
   return (
     <table className="tableDefault">
@@ -15,8 +29,6 @@ const MediaTable = ({mediaArray}) => {
   );
 };
 
-MediaTable.propTypes = {
-  mediaArray: PropTypes.array.isRequired,
-};
+MediaTable.propTypes = {};
 
 export default MediaTable;
