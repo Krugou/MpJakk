4b60798622c1f3b84afb5af1ab75bc2b0a19258a
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-09 09:03:08 +0200

    asd

---
+++ src/hooks/ApiHooks.js
@@ -0,0 +1,39 @@
+import {useEffect, useState} from 'react';
+import {baseUrl} from '../utils/variables';
+const fetchJson = async (url, options = {}) => {
+  try {
+    const response = await fetch(url, options);
+    const json = await response.json();
+    if (response.ok) {
+      return json;
+    } else {
+      const message = json.message;
+      throw new Error(message);
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+  const getMediaFetch = async () => {
+    try {
+      const media = await fetchJson(baseUrl + 'media');
+      const allFiles = await Promise.all(
+        media.map(async (file) => {
+          return await fetchJson(`${baseUrl}media/${file.file_id}`);
+        })
+      );
+      setMediaArray(allFiles);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getMediaFetch();
+  }, []);
+  return {mediaArray};
+};
+
+export {useMedia};
