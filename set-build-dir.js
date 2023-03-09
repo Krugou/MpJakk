02d468487133e4c07f4c6166f8ba18966b3bd78c
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-09 02:48:18 +0200

    Update set-build-dir.js

--- set-build-dir.js
+++ set-build-dir.js
@@ -22,5 +22,5 @@ try {
   console.error(`Error reading package.json file: ${err}`);
   process.exit(1);
 }
-packageJson.scripts.build = `set BUILD_PATH=${buildDirName} &&  react-scripts build`;
+packageJson.scripts.buildBranch = `set BUILD_PATH=${buildDirName} &&  react-scripts build`;
 fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
