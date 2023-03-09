04cba4d323e794b570d7f59c8d844cc6af0f115f
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-09 03:53:53 +0200

    Update set-build-dir.js

--- set-build-dir.js
+++ set-build-dir.js
@@ -1,10 +1,11 @@
 const {execSync} = require('child_process');
+const fs = require('fs');
 
 // Get the current branch name
 const branchName = execSync('git rev-parse --abbrev-ref HEAD')
   .toString()
   .trim();
-console.log(`Current branch name: ${branchName}`);
+console.log(`Current branch name: "${branchName}"`);
 // Set the build directory name based on the branch name
 let buildDirName;
 if (branchName === 'main') {
@@ -12,9 +13,23 @@ if (branchName === 'main') {
 } else {
   buildDirName = `mpjakkpages/${branchName}`;
 }
+// Read the contents of the public/index.html file
+fs.readFile('public/index.html', 'utf8', (err, data) => {
+  if (err) throw err;
 
+  // Replace the title tag with the branchname variable
+  const updatedData = data.replace(
+    /<title>.*<\/title>/,
+    `<title>${branchName}</title>`
+  );
+
+  // Write the updated contents back to the public/index.html file
+  fs.writeFile('public/index.html', updatedData, 'utf8', (err) => {
+    if (err) throw err;
+    console.log('index.html file updated successfully');
+  });
+});
 // Update the "build" field in package.json
-const fs = require('fs');
 let packageJson;
 try {
   packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
