4600be28b3c91e3c5aa37ae9e86a843f90e71c95
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-09 02:36:59 +0200

    kustom

---
+++ set-build-dir.js
@@ -0,0 +1,30 @@
+const {execSync} = require('child_process');
+
+// Get the current branch name
+const branchName = execSync('git rev-parse --abbrev-ref HEAD')
+  .toString()
+  .trim();
+console.log(`Current branch name: ${branchName}`);
+// Set the build directory name based on the branch name
+let buildDirName;
+if (branchName === 'main') {
+  buildDirName = 'build';
+} else {
+  buildDirName = `mpjakkpages/${branchName}`;
+}
+
+// Update the "build" field in package.json
+const fs = require('fs');
+let packageJson;
+try {
+  packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+  console.log(
+    '🚀 ~ file: set-build-dir.js:21 ~ packageJson:',
+    packageJson.scripts
+  );
+} catch (err) {
+  console.error(`Error reading package.json file: ${err}`);
+  process.exit(1);
+}
+packageJson.scripts.build = `node set-build-dir.js && set BUILD_PATH=${buildDirName} &&  react-scripts build`;
+fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
