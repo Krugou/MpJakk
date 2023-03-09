42a56d83406af21b8156a6e0c8dca9efbfddbd03
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-09 09:23:08 +0200

    asd

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Profile</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
