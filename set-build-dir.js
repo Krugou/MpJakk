c80e60e5bb634704821d2ef246d3b03151df1524
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-10 02:12:28 +0200

    asd

--- set-build-dir.js
+++ set-build-dir.js
@@ -1,11 +1,12 @@
 const {execSync} = require('child_process');
 const fs = require('fs');
-
+const path = require('path');
 // Get the current branch name
 const branchName = execSync('git rev-parse --abbrev-ref HEAD')
   .toString()
   .trim();
 console.log(`Current branch name: "${branchName}"`);
+
 // Set the build directory name based on the branch name
 let buildDirName;
 if (branchName === 'main') {
@@ -13,6 +14,7 @@ if (branchName === 'main') {
 } else {
   buildDirName = `mpjakkpages/${branchName}`;
 }
+
 // Read the contents of the public/index.html file
 fs.readFile('public/index.html', 'utf8', (err, data) => {
   if (err) throw err;
@@ -39,3 +41,18 @@ try {
 }
 packageJson.scripts.buildBranch = `set BUILD_PATH=${buildDirName}&&  react-scripts build`;
 fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
+
+// Generate link to deployment
+const link = `https://users.metropolia.fi/~aleksino/mpjakk/${branchName}`;
+
+// Write link to README.md
+const readmePath = './README.md';
+const readmeContent = fs.readFileSync(readmePath, 'utf8');
+const lines = readmeContent.split('\n');
+if (lines.length < 13) {
+  console.error('README.md file does not contain expected number of lines');
+  process.exit(1);
+}
+lines[12] = `Open link in browser [${link}](${link})`;
+fs.writeFileSync(readmePath, lines.join('\n'));
+console.log('Link added to README.md');
