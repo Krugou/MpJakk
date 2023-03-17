64f16aa498699caf85151d53a940ebd8bfd1e9c8
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-17 10:25:41 +0200

    asd

--- src/components/MediaTable.jsx
+++ src/components/MediaTable.jsx
@@ -1,36 +1,15 @@
 // import PropTypes from 'prop-types';
-import React, {useEffect, useState} from 'react';
-import {baseUrl} from '../utils/variables';
+// import {useEffect, useState} from 'react';
+// import {baseUrl} from '../utils/variables';
+import {useMedia} from '../hooks/ApiHooks';
 import MediaRow from './MediaRow';
-console.log('🚀 ~ file: MediaTable.js:5 ~ baseUrl:', baseUrl);
 
 const MediaTable = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-  const getMediaFetch = async () => {
-    try {
-      const mediaResponse = await fetch(baseUrl + 'media');
-      const media = await mediaResponse.json();
-
-      const allFiles = await Promise.all(
-        media.map(async (file) => {
-          const fileResponse = await fetch(`${baseUrl}media/${file.file_id}`);
-
-          return await fileResponse.json();
-        })
-      );
-
-      setMediaArray(allFiles);
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
-  useEffect(() => {
-    getMediaFetch();
-  }, []);
-
+  const {mediaArray} = useMedia();
   console.log(mediaArray);
+
   return (
-    <table className="tableDefault">
+    <table>
       <tbody>
         {mediaArray.map((item, index) => {
           return <MediaRow key={index} file={item} />;
