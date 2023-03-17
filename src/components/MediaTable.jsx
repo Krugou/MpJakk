6810563e56e4a723499e868f0e232a3fc1a1e39c
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-17 11:22:19 +0200

    asd

--- src/components/MediaTable.jsx
+++ src/components/MediaTable.jsx
@@ -1,6 +1,13 @@
+// import PropTypes from 'prop-types';
+// import {useEffect, useState} from 'react';
+// import {baseUrl} from '../utils/variables';
+import {useMedia} from '../hooks/ApiHooks';
 import MediaRow from './MediaRow';
 
 const MediaTable = () => {
+  const {mediaArray} = useMedia();
+  console.log(mediaArray);
+
   return (
     <table>
       <tbody>
